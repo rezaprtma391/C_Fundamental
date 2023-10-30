18dd53312da4553e913aa32173367118aa3e9708
Muhamad Reza Pratama <rezaprtma391@gmail.com>
2023-10-30 16:01:08 +0700

    first-commit

---
+++ halo.c
@@ -0,0 +1,30 @@
+//Tanpa Nilai Kembalian
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+	
+//}
+
+//Nilai Kemabalian
+//#include <stdio.h>
+//int main ()
+//{
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+	
+//	return 0;
+//}
+
+#include <stdio.h>
+int main () {
+	int i = 5;
+	printf ("ini nilai i : %d \n", i);
+	return 0;
+}
+
+
+
